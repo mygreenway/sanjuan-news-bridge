02f899c1ce417074b66c04cebc79a6d64d2343b9
mygreenway <94966998+mygreenway@users.noreply.github.com>
2025-05-02 23:28:40 +0200

    bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import os
+import asyncio
 from telegram import Bot
 from telegram.constants import ParseMode
 
@@ -7,9 +8,12 @@ channel_id = "@sanjuan_online"
 
 bot = Bot(token=bot_token)
 
-# Тестовое сообщение
-bot.send_message(
-    chat_id=channel_id,
-    text="✅ *Тестовая публикация:* San Juan Bot успешно запущен!",
-    parse_mode=ParseMode.MARKDOWN
-)
+async def main():
+    await bot.send_message(
+        chat_id=channel_id,
+        text="✅ *Тестовая публикация:* San Juan Bot успешно запущен!",
+        parse_mode=ParseMode.MARKDOWN
+    )
+
+if __name__ == "__main__":
+    asyncio.run(main())
